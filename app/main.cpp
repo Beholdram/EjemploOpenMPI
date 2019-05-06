3f8760eaf0b1805037ef460bba5553fbe48062f9
ElvenDreadnought <imusen.lyce@hotmail.com>
2019-05-05 21:51:16 -0400

    Update main.cpp
    Ahora da el nombre y la fecha

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> /*para concatenar hostname y fecha con hora*/
 #include <unistd.h>
 
 /* cabecera de las llamadas a MPI */
@@ -20,8 +20,8 @@ int main(int argc, char** argv) {
     int fuente; /* rango del que envia  */
     int dest; /* rango del que recibe */
     int tag = 0; /* etiqueta del mensaje */
-    char mensaje[228]; /* mensaje  */
-    char hostname[128]; /*Para guardar el nombre de la máquina*/
+    char mensaje[328]; /* mensaje  */
+    char hostname[228]; /*Para guardar el nombre de la máquina*/
     MPI_Status estado; /* devuelve estado al recibir*/
 
     /* Comienza las llamadas a MPI */
@@ -37,29 +37,26 @@ int main(int argc, char** argv) {
 
     if (mi_rango != 0) {
         /* Crea mensaje */
-        gethostname(hostname, sizeof hostname); 
-        /*Ahora estoy haciendo que cada proce que no sea el master imprima el nombre de la máquina*/
-        sprintf(mensaje, " Hola! Mi nombre es %s!", hostname);
+        time_t current_time; /*Para guardar una fecha y hora que no están en lenguaje humano*/
+        char* c_time_string; /*Para guardar la versión legible por humanos*/
+        current_time = time(NULL); /*Obtiene la versión ilegible por humanos*/
+        c_time_string = ctime(&current_time); /*Obtiene la versión legible por humanos*/
+        gethostname(hostname, sizeof hostname);/*Obtiene el nombre de la máquina*/
+        /*con el nombre de la máquina en un arreglo y una fecha y hora en otro, los convierto en uno*/
+        strcat(hostname," |");strcat(hostname,c_time_string);/*strcat(hostname,"].");*/
+        /*Me falta leer más documentación sobre esta forma de obtener la fecha y la hora porque viene con un salto de linea que no comprendo...*/
+        sprintf(mensaje, " Hola, mi nombre es %s", hostname);
         dest = 0;
         /* Usa strlen+1 para que la marca /0 se transmita */
         MPI_Send(mensaje, strlen(mensaje) + 1, MPI_CHAR,
                 dest, tag, MPI_COMM_WORLD);
     } else { /* mi_rango == 0 */
         for (fuente = 1; fuente < p; fuente++) {
-            MPI_Recv(mensaje, 228, MPI_CHAR, fuente,
+            MPI_Recv(mensaje, 328, MPI_CHAR, fuente,
                     tag, MPI_COMM_WORLD, &estado);
             printf("%s\n", mensaje);
         }
     }
-
-    /* Termina con MPI. Recordemos que después de 
-     * esta llamada no podemos llamar a funciones 
-     * MPI, ni siquiera de nuevo a MPI_Init 
-     */
-    MPI_Finalize();
-    return EXIT_SUCCESS;
-}
-
     /* Termina con MPI. Recordemos que después de 
      * esta llamada no podemos llamar a funciones 
      * MPI, ni siquiera de nuevo a MPI_Init 
